85916f1cea30c818bbb41e958b71ee20856861eb
barin2010 <uyrirudenko@gmail.com>
2023-10-05 15:36:48 +0300

    Update_task_02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+const listItems = [];
+
+ingredients.forEach(function(ingredient) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+  listItems.push(listItem);
+});
+
+ingredientsList.append(...listItems);
